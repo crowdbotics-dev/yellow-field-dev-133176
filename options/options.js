0c702ae16f6f0b96f624677bee18b76de1a35bfa
Crowdbotics <engineering@crowdbotics.com>
2024-02-09 18:31:40 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "yellow-field-dev-133176",
+  url: "https://yellow-field-dev-133176.botics.co",
+  api: "https://yellow-field-dev-133176.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
